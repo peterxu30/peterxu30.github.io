b1e43e4697cc9f4d861121c9aa9e46c189b9fa02
peterxu30 <peterxu30@gmail.com>
2015-07-13 12:38:39 -0700

    Updated quotes

--- js/QuoteChange.js
+++ js/QuoteChange.js
@@ -2,6 +2,7 @@ var quotes =
 	["The more I change, the less I stay the same.",
 	 "When you do things right, people won't be sure you've done anything at all. - Futurama",
 	 "Those who knew him, loved him, while those who did not know him, loved him from afar. - TNG",
+	 "Truly, they were as gods who built this place! - Futurama",
 	 "You do you."];
 
 function changeQuote() {
